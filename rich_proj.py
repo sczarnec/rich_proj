5b192b8d7cbeadfdde45717cc575fc70916ff7cc
sczarnec <sczarnecki1212@gmail.com>
2025-11-10 22:27:42 -0600

    Add filters

--- rich_proj.py
+++ rich_proj.py
@@ -18,7 +18,31 @@ schedule_grouped = (
     .reset_index()
     .sort_values(["season", "week"], ascending=False)
 )
+
+unique_seasons = sorted(schedule_grouped['season'].unique(), reverse=True)
+season_options = st.multiselect(
+    "Season", 
+    options=["All"] + unique_seasons,  
+    default = ["All"] 
+)
+
+unique_weeks = sorted(schedule_grouped['week'].unique(), reverse=True)
+week_options = st.multiselect(
+    "Week", 
+    options=["All"] + unique_weeks,  
+    default = ["All"] 
+)
+
+
+st.write(" ")
+
 schedule_grouped = schedule_grouped.loc[schedule_grouped["total_pts"]!=0]
+
+if "All" not in season_options:
+    schedule_grouped = schedule_grouped[schedule_grouped["season"].isin(season_options)]
+
+if "All" not in week_options:
+    schedule_grouped = schedule_grouped[schedule_grouped["week"].isin(week_options)]
 
 
 
